c4da47f76ee4c7c3bd22c68e1e56efee26f83e9d
Observed Observer <MasterHaoChen@icloud.com>
2018-09-10 20:31:48 +0800

    样式图片调整

--- src/pages/intro/index.vue
+++ src/pages/intro/index.vue
@@ -5,7 +5,7 @@
         Phantom Tiger
       </el-header>
       <el-main class="banner-segment">
-        <img src="//localhost/dataviz.png" alt="">
+        <img src="//powerbicdn.azureedge.net/cvt-d6431fcc8bafcfb914f233b33206ee2aa0f89088a3dce2aac8f31d0f60cf390d/pictures/pages/powerbi-desktop-hero.gif?636718490568034631" alt="">
         <div class="banner-content">
           <h1>Phantom BI</h1>
           <p>服务于工业大数据自主分析，基于自由和开放的交互理念，让分析师更专注于数据背后的故事。</p>
@@ -48,8 +48,8 @@ html body{
   .banner-content {
     margin-top: 100px;
     width: 48%;
-    min-width: 560px;
-    max-width: 420px;
+    min-width: 500px;
+    max-width: 400px;
     min-height: 320px;
     color: #0d1a26;
   }
@@ -70,11 +70,8 @@ html body{
   }
   .banner-segment img {
     position: absolute;
-    transform: scaleX(-1);
     z-index: -1;
-    left: -5%;
-    top: -30%;
-
+    left: 40%;
   }
   /* .rotate-segment{
     position: absolute;
